af3399e632b42f70ed6db6ce0bbf00ef2e0df592
Rahulgoy <rahulkumargoyal00@gmail.com>
2021-12-30 21:49:50 +0530

    Optimized

--- attendace.py
+++ attendace.py
@@ -4,6 +4,7 @@ import face_recognition
 import os
 import numpy as np
 from datetime import datetime
+import pickle
 
 path = 'Images'
 
@@ -17,15 +18,45 @@ for cls in myList:
     images.append(curImg)
     classNames.append(os.path.splitext(cls)[0])
 
-print(classNames)
+
+print(len(images))
 def findencodings(images):
     encodeList = []
+    count=0
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
+        encode = face_recognition.face_encodings(img)
+        # print(encode)
+        if encode:
+            # print(encode)
+            encodeList.append(encode[0])
+        else:
+            print(classNames[count])
+            # print(img)
+        count=count+1
     return encodeList
 
+
+try:
+    # face_name= np.loadtxt('face_names.csv',delimiter=',')
+    with open('face_names.pkl','rb') as f: face_name = pickle.load(f)
+    with open('encodings.pkl','rb') as f: encodeListKnown = pickle.load(f)
+    print("Try block")
+    if len(face_name)<len(classNames):
+        encodeListKnown = findencodings(images)
+        classNames=face_name
+except:
+    print("except block")
+    encodeListKnown = findencodings(images)
+    with open('face_names.pkl','wb') as f: 
+        pickle.dump(classNames, f)
+    with open('encodings.pkl','wb') as f: 
+        pickle.dump(encodeListKnown, f)
+
+
+
+# print(classNames)
+# print(face_name)
 
 def markAttendance(name):
     with open('Attendance.csv','r+') as f:
@@ -40,8 +71,18 @@ def markAttendance(name):
             f.writelines(f'\n{name},{dtstring}')
 
 
-encodeListKnown = findencodings(images)
-# print(len(encodeListKnown))
+# encodeListKnown = findencodings(images)
+# data = np.asarray(encodeListKnown)
+# print(encodeListKnown)
+# np.savetxt('test.csv', data, delimiter=',')
+
+# face_names = np.asarray(classNames)
+# np.savetxt('face_names.csv', face_names, delimiter=',')
+# encodeListKnown= np.loadtxt('test.csv',delimiter=',')
+# print(data_new)
+
+
+print(encodeListKnown)
 print("Encoding Complete")
 
 cap = cv2.VideoCapture(0)
@@ -51,7 +92,6 @@ while True:
     # img = cv2.imread("Rahul.jpeg")
     red_img = cv2.resize(img, (0,0),None,0.25,0.25)
     red_img = cv2.cvtColor(red_img,cv2.COLOR_BGR2RGB)
-    
 
     face_location = face_recognition.face_locations(red_img)
     # print(face_location)
